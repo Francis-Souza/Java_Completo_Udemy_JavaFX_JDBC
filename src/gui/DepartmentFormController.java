280093b387cc952becfcf97c3887feb0b52826bb
Francis-Souza <francisleisouza@gmail.com>
2021-10-04 14:53:08 -0300

    DepartmentFormController

---
+++ src/gui/DepartmentFormController.java
@@ -0,0 +1,53 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable {
+	
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtNameDepartment;
+	
+	@FXML
+	private Button btnSave;
+	
+	@FXML
+	private Button btnCancel;
+	
+	@FXML
+	private Label lxlError;
+	
+	@FXML
+	public void onBtnSaveAction() {		
+		System.out.println("Save");		
+	}
+	
+	@FXML
+	public void onBtnCancelAction() {
+		System.out.println("Cancel");		
+	}
+	
+	@Override	
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		initializeNodes();				
+	}
+	
+	
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtNameDepartment, 30);
+	}
+
+}
